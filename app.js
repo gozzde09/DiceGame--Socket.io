433d9923e2864e1c025678ef5a327abfdb7d459b
Gzd Akgn <gozzde0990@gmail.com>
2025-08-25 21:32:38 +0200

    express

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const io = new Server(server);
 const port = 3000;
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 // Mongoose
 const MessageModel = require("./models/messageModel");
 const ResultModel = require("./models/resultModel");
