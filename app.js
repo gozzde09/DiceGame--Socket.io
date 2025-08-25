2f8f341ee9833fa9ced0602d6138959bf5ca89fe
Gzd Akgn <gozzde0990@gmail.com>
2025-08-25 21:41:35 +0200

    serve frontend

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("."));
+
 // Mongoose
 const MessageModel = require("./models/messageModel");
 const ResultModel = require("./models/resultModel");
